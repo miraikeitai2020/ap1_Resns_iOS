41caa4b094e816223c6ea3135a9cbba823c3138b
shimotahirayawaragi <s1821071@cco.kanagawa-it.ac.jp>
2020-10-26 21:56:05 +0900

    add

---
+++ Resns/View/Components/NewsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  NewsCollectionViewCell.swift
+//  Resns
+//
+//  Created by shimotahira on 2020/10/26.
+//
+
+import UIKit
+
+class NewsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Resns/ViewController/Login_After/News/NewsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  NewsViewController.swift
+//  Resns
+//
+//  Created by shimotahira on 2020/10/26.
+//
+
+import Foundation
